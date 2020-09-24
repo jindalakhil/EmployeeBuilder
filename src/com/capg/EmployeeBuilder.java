e63f5c2ddd3285474ffdcab0f36ab8bafc7f2114
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 11:54:55 +0530

    [Akhil Jindal]Added: Employee present or absent

--- src/com/capg/EmployeeBuilder.java
+++ src/com/capg/EmployeeBuilder.java
@@ -1,5 +1,15 @@
 package com.capg;
 
 public class EmployeeBuilder {
-
+	public static void main(String[] args) {
+		//Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Emploee is absent");
+		}
+	}
 }
