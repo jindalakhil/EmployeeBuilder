66c632515526f678c6c0c645421aff9b12955786
jindalakhil <akhiljindal222@gmail.com>
2020-09-28 16:20:38 +0530

    [Akhil Jindal]Added: Ability to compute wage for multiple companies

--- src/com/capg/EmployeeBuilder.java
+++ src/com/capg/EmployeeBuilder.java
@@ -3,16 +3,14 @@ package com.capg;
 public class EmployeeBuilder {
 	public static final int IS_PART_TIME =1;
 	public static final int IS_FULL_TIME =2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-	
-	public static int computeWage() {
+
+	public static int computeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		int empHrs = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
+		int totalEmpWage = 0;
 		
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -25,10 +23,11 @@ public class EmployeeBuilder {
 			default:
 				empHrs = 0;
 			}
-			totalEmpHrs +=empHrs;
-			System.out.println("DAY#: " + totalWorkingDays + "Emp Hr: "+ empHrs);
-		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		return totalEmpWage;
+			totalEmpHrs += empHrs;
+			System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
+			}
+			totalEmpWage = totalEmpHrs * empRatePerHour;
+			System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
+			return totalEmpWage;
 	}
 }
--- src/com/capg/EmployeeExecutor.java
+++ src/com/capg/EmployeeExecutor.java
@@ -3,6 +3,7 @@ package com.capg;
 public class EmployeeExecutor {
 	public static void main(String args[]) {
 		EmployeeBuilder obj = new EmployeeBuilder();
-		System.out.println("Total Emp Wage: "+ obj.computeWage());
+		System.out.println("Total Emp Wage: "+ obj.computeWage("Reliance", 15, 25 , 100));
+		System.out.println("Total Emp Wage: "+ obj.computeWage("BigBazar", 20, 27 , 90));	
 	}
 }
